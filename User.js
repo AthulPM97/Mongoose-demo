670085d8046ad821fc20196fec6b34982c612744
athulpm <97athulpm@gmail.com>
2023-04-28 20:55:07 +0530

    Updated user schema and validation

--- User.js
+++ User.js
@@ -1,9 +1,26 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    name: String,
-    age: Number,
+  name: String,
+  age: {
+    type: Number,
+    min: 1,
+    max: 100,
+    validate: {
+      //will validate the value passed in for age
+      validator: (v) => v % 2,
+      message: (props) => `${props.value} is not an even number`,
+    },
+  },
+  email: String,
+  createdAt: { type: Date, default: () => Date.now(), immutable: true },
+  updatedAt: { type: Date, default: () => Date.now() },
+  bestFriend: mongoose.SchemaTypes.ObjectId,
+  hobbies: [String],
+  address: {
+    street: String,
+    city: String,
+  },
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-const mongoose = require('mongoose');
-const User = require('./User');
+const mongoose = require("mongoose");
+const User = require("./User");
 
+mongoose.connect("mongodb://127.0.0.1:27017/testdb");
 
-mongoose.connect('mongodb://localhost/testdb');
-
-const user = new User({name: 'athul', age: 25});
-user.save().then(() => console.log('user saved'))
+run();
+async function run() {
+  try {
+    const user = await User.create({
+      name: "athul",
+      age: 25,
+      email: "athul@gmail.com",
+    });
+    console.log(user);
+  } catch (err) {
+    console.log(err.message);
+  }
+}
