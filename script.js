0023f59061f7ed72af04139ca3a05359d2f9714e
athulpm <97athulpm@gmail.com>
2023-04-29 22:33:40 +0530

    chaining queries

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ mongoose.connect("mongodb://127.0.0.1:27017/testdb");
 run();
 async function run() {
   try {
-    const user = User.findOne().where({name: 'athul'}).limit(1);
+    // const user = User.findOne().where({name: 'athul'}).where().limit(1);
+    const user = await User.where('name').equals('athul').limit(1).select('age')
     console.log(user)
   } catch (err) {
     console.log(err.message);
